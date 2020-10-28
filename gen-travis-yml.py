d5630788aabaa26b4ffc5b6ece14f0206f571d2d
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-10-28 00:50:25 -0400

    travis-ci: deploy only on final job

--- gen-travis-yml.py
+++ gen-travis-yml.py
@@ -138,13 +138,13 @@ for i in range(0, len(manifest), build_stage_size):
         - .travis.d/build.sh "{p}"''')
 
 print('''\
-deploy:
-  provider: releases
-  api_key:
-    secure: Iq9pGKXRKBlW45VdSNPmHq/atWPebPmj5J6jo1aIrf/TE2mQSyp/b2qMO7Q1s0gEMi4AQRKUO5/iugWtEjK4puA/3uKtlwA6D4jlZr0VdD3i8jD8a0npLRT3ESGoSVul1N5hWov+2AfJYbIq7BgfrqpVPTTMaYx3vA9FyJkzBTieVXvblIe0qoViisjQNLQftSwDyLLgoEAZ8AIbX+WQI7jmpDumP/uIVJb7IVwMbXol2vIWXCtaivmy9F4/btYJLb5EeH6gXlpnYeGai8aRqwBnHLKMR0sGsQKAJfajOCxuqfFjtrUSypFlVNAUqsN0L16dsRN49/fI+SofHnpyKxcd/tH9MrRcoGS6hzzE5sP/dRPCONdRVRyeTi3YkJ6P8+iqoZzecOSlqAaeqfP3GO1NaZZSTt22BZzRJw3LM0HZ78e4MRvh+7+VCHX4rQnwFA2QrjfSDjV9+xErDjo/f+zgVvu5dbCknFG/taeAPy1poIQ7G3xN/emkV0HHyDchyup6Qtmlz21RpRDytRtN3ACsBGLqxYznc7o/9bMfS9swxB7jbfeOg1LQ2jym2JvKv8rai7H3F+SkNV6LOCrFq3XYr1AnEPFvbcRZDbdIj3NjilPAf45R1tYzGIxGSqbCgCwan8tFJ2jI78KevqV0CweD0wTtDmrSNFRPuCj3Eeo=
-  file_glob: true
-  file: deploy/*
-  skip_cleanup: true
-  on:
-    tags: true
-    repo: DolceSDK/packages''')
+      deploy:
+        provider: releases
+        api_key:
+          secure: Iq9pGKXRKBlW45VdSNPmHq/atWPebPmj5J6jo1aIrf/TE2mQSyp/b2qMO7Q1s0gEMi4AQRKUO5/iugWtEjK4puA/3uKtlwA6D4jlZr0VdD3i8jD8a0npLRT3ESGoSVul1N5hWov+2AfJYbIq7BgfrqpVPTTMaYx3vA9FyJkzBTieVXvblIe0qoViisjQNLQftSwDyLLgoEAZ8AIbX+WQI7jmpDumP/uIVJb7IVwMbXol2vIWXCtaivmy9F4/btYJLb5EeH6gXlpnYeGai8aRqwBnHLKMR0sGsQKAJfajOCxuqfFjtrUSypFlVNAUqsN0L16dsRN49/fI+SofHnpyKxcd/tH9MrRcoGS6hzzE5sP/dRPCONdRVRyeTi3YkJ6P8+iqoZzecOSlqAaeqfP3GO1NaZZSTt22BZzRJw3LM0HZ78e4MRvh+7+VCHX4rQnwFA2QrjfSDjV9+xErDjo/f+zgVvu5dbCknFG/taeAPy1poIQ7G3xN/emkV0HHyDchyup6Qtmlz21RpRDytRtN3ACsBGLqxYznc7o/9bMfS9swxB7jbfeOg1LQ2jym2JvKv8rai7H3F+SkNV6LOCrFq3XYr1AnEPFvbcRZDbdIj3NjilPAf45R1tYzGIxGSqbCgCwan8tFJ2jI78KevqV0CweD0wTtDmrSNFRPuCj3Eeo=
+        file_glob: true
+        file: deploy/*
+        skip_cleanup: true
+        on:
+          tags: true
+          repo: DolceSDK/packages''')
