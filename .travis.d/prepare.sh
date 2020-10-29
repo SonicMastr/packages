897bef0dede39c182f00513b505da8c7668b36d7
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-10-28 21:45:51 -0400

    travis-ci: authenticate Github API access

--- .travis.d/prepare.sh
+++ .travis.d/prepare.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+if [ -n "$AUTH" ]; then
+	AUTH="--auth $AUTH"
+fi
+
 git clone --depth=1 https://github.com/DolceSDK/ddpm.git
-python3 ddpm/dolcesdk-update.py
+python3 ddpm/dolcesdk-update.py $AUTH
 rm -rf ddpm
 mkdir deploy
